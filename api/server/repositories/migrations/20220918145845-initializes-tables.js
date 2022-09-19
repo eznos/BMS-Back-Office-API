07a3b710310c2618580c1a12831163a3a94742fe
Vasuphol <baigho123@gmail.com>
2022-09-19 19:40:56 +0700

    build  🔨: create all table in db

--- api/server/repositories/migrations/20220918145845-initializes-tables.js
+++ api/server/repositories/migrations/20220918145845-initializes-tables.js
@@ -1,123 +1,407 @@
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable('users', {
+        await queryInterface.createTable(
+            "users",
+            {
+                id: {
+                    unique: true,
+                    allowNull: false,
+                    primaryKey: true,
+                    type: DataTypes.UUID,
+                    defaultValue: DataTypes.UUIDV4,
+                    field: "id",
+                    comment: "ไอดีของตาราง",
+                },
+                username: {
+                    allowNull: false,
+                    type: Sequelize.STRING,
+                    field: "username",
+                },
+                password: {
+                    allowNull: false,
+                    type: Sequelize.STRING,
+                    field: "password",
+                },
+                otpSecret: {
+                    type: Sequelize.STRING,
+                    field: "otp_secret",
+                },
+                role: {
+                    allowNull: false,
+                    type: DataTypes.ENUM,
+                    values: ["admin", "user"],
+                    field: "role",
+                },
+                rank: {
+                    type: DataTypes.ENUM,
+                    values: [
+                        "n/a",
+                        "พล.ต.ต.",
+                        "พ.ต.อ.",
+                        "พ.ต.ท.",
+                        "พ.ต.ต.",
+                        "ร.ต.อ.",
+                        "ร.ต.ท.",
+                        "ร.ต.ต.",
+                        "ด.ต.",
+                        "จ.ส.ต.",
+                        "ส.ต.อ.",
+                        "ส.ต.ท.",
+                        "ส.ต.ต.",
+                    ],
+                    field: "rank",
+                },
+                affiliation: {
+                    type: DataTypes.ENUM,
+                    values: [
+                        "n/a",
+                        "ผบช.ภ.3",
+                        "สนง.ผบช.ภ.3",
+                        "สนง.รอง ผบช.ภ.3",
+                        "ภ.3(ส่วนกลาง)",
+                        "บก.สส.ภ.3",
+                        "ภ.จว.นม.",
+                        "สภ.เมืองนครราชสีมา",
+                        "บก.อก.ภ.3",
+                        "ศพฐ.3",
+                        "ปฏิบัติราชการ",
+                        "ประจำ",
+                        "สำรอง",
+                        "ภ.3",
+                        "ศฝร.ภ.3",
+                    ],
+                    field: "affiliation",
+                },
+                firstName: {
+                    type: Sequelize.STRING,
+                    field: "first_name",
+                },
+                lastName: {
+                    type: Sequelize.STRING,
+                    field: "last_name",
+                },
+                gender: {
+                    type: DataTypes.ENUM,
+                    values: ["n/a", "male", "female"],
+                    field: "gender",
+                },
+                email: {
+                    unique: true,
+                    type: Sequelize.STRING,
+                    field: "email",
+                },
+                phoneNumber: {
+                    type: Sequelize.STRING,
+                    field: "phone_number",
+                },
+                profileUrl: {
+                    type: Sequelize.STRING,
+                    field: "profile_url",
+                },
+                deleted: {
+                    type: Sequelize.BOOLEAN,
+                    field: "deleted",
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE,
+                    defaultValue: DataTypes.NOW,
+                    field: "created_at",
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE,
+                    defaultValue: DataTypes.NOW,
+                    field: "updated_at",
+                },
+            },
+            {
+                indexes: [{ unique: true, fields: ["username", "email"] }],
+            }
+        );
+        await queryInterface.createTable("accommodations", {
             id: {
                 unique: true,
                 allowNull: false,
                 primaryKey: true,
                 type: DataTypes.UUID,
                 defaultValue: DataTypes.UUIDV4,
-                field: 'id',
-                comment: 'ไอดีของตาราง'
+                field: "id",
+                comment: "ไอดีของตาราง",
             },
-            username: {
-                allowNull: false,
-                type: Sequelize.STRING,
-                field: 'username'
+            roomId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "room_id",
             },
-            password: {
-                allowNull: false,
-                type: Sequelize.STRING,
-                field: 'password'
+            userId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "user_id",
             },
-            otpSecret: {
-                type: Sequelize.STRING,
-                field: 'otp_secret'
-            },
-            role: {
-                allowNull: false,
-                type: DataTypes.ENUM,
-                values: ['admin', 'user'],
-                field: 'role'
-            },
-            rank: {
-                type: DataTypes.ENUM,
-                values: ['n/a',
-                    'พล.ต.ต.',
-                    'พ.ต.อ.',
-                    'พ.ต.ท.',
-                    'พ.ต.ต.',
-                    'ร.ต.อ.',
-                    'ร.ต.ท.',
-                    'ร.ต.ต.',
-                    'ด.ต.',
-                    'จ.ส.ต.',
-                    'ส.ต.อ.',
-                    'ส.ต.ท.',
-                    'ส.ต.ต.'],
-                field: 'rank'
-            },
-            affiliation: {
-                type: DataTypes.ENUM,
-                values: ['n/a',
-                    'ผบช.ภ.3',
-                    'สนง.ผบช.ภ.3',
-                    'สนง.รอง ผบช.ภ.3',
-                    'ภ.3(ส่วนกลาง)',
-                    'บก.สส.ภ.3',
-                    'ภ.จว.นม.',
-                    'สภ.เมืองนครราชสีมา',
-                    'บก.อก.ภ.3',
-                    'ศพฐ.3',
-                    'ปฏิบัติราชการ',
-                    'ประจำ',
-                    'สำรอง',
-                    'ภ.3',
-                    'ศฝร.ภ.3'],
-                field: 'affiliation'
-            },
-            firstName: {
-                type: Sequelize.STRING,
-                field: 'first_name'
-            },
-            lastName: {
-                type: Sequelize.STRING,
-                field: 'last_name'
-            },
-            gender: {
-                type: DataTypes.ENUM,
-                values: ['n/a', 'male', 'female'],
-                field: 'last_name'
-            },
-            email: {
-                unique: true,
-                type: Sequelize.STRING,
-                field: 'email'
-            },
-            phoneNumber: {
-                type: Sequelize.STRING,
-                field: 'phone_number'
-            },
-            profileUrl: {
-                type: Sequelize.STRING,
-                field: 'profile_url'
+            host: {
+                type: Sequelize.BOOLEAN,
+                field: "host",
             },
             deleted: {
                 type: Sequelize.BOOLEAN,
-                field: 'deleted'
+                field: "deleted",
             },
             createdAt: {
                 allowNull: false,
                 type: Sequelize.DATE,
                 defaultValue: DataTypes.NOW,
-                field: 'created_at'
+                field: "created_at",
             },
             updatedAt: {
                 allowNull: false,
                 type: Sequelize.DATE,
                 defaultValue: DataTypes.NOW,
-                field: 'updated_at'
-            }
-        }, {
-            indexes: [{unique: true, fields: ['username', 'email']}]
-        })
+                field: "updated_at",
+            },
+        });
+        await queryInterface.createTable("billings", {
+            id: {
+                unique: true,
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "id",
+                comment: "ไอดีของตาราง",
+            },
+            billingType: {
+                type: DataTypes.ENUM,
+                values: ["electricity", "water"],
+                field: "billing_type",
+            },
+            accommodationId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "accommodation_id",
+                comment: "ไอดีของตารางaccommodation ที่เป็นคีย์นอก",
+            },
+            status: {
+                type: DataTypes.ENUM,
+                values: ["draft", "in_progess", "calculated", "exported"],
+                field: "status",
+            },
+            unit: {
+                type: DataTypes.INTEGER,
+                field: "unit",
+            },
+            price: {
+                type: DataTypes.FLOAT,
+                field: "price",
+            },
+            priceDiff: {
+                type: DataTypes.FLOAT,
+                field: "price_diff",
+            },
+            totalPay: {
+                type: DataTypes.FLOAT,
+                field: "total_pay",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "created_at",
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "updated_at",
+            },
+        });
+        await queryInterface.createTable("zones", {
+            id: {
+                unique: true,
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "id",
+                comment: "ไอดีของตาราง",
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "name",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "created_at",
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "updated_at",
+            },
+        });
+        await queryInterface.createTable("water_zones", {
+            id: {
+                unique: true,
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "id",
+                comment: "ไอดีของตาราง",
+            },
+            zoneId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "zone_id",
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "name",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "created_at",
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "updated_at",
+            },
+        });
+        await queryInterface.createTable("buildings", {
+            id: {
+                unique: true,
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "id",
+                comment: "ไอดีของตาราง",
+            },
+            zoneId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "zone_id",
+            },
+            waterZoneId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "water_zone_id",
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "name",
+            },
+            imageUrl: {
+                type: DataTypes.STRING,
+                field: "image_url",
+            },
+            lat: {
+                type: DataTypes.DOUBLE,
+                field: "lat",
+            },
+            lng: {
+                type: DataTypes.DOUBLE,
+                field: "lng",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "created_at",
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "updated_at",
+            },
+        });
+        await queryInterface.createTable("buildings", {
+            id: {
+                unique: true,
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "id",
+                comment: "ไอดีของตาราง",
+            },
+            zoneId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "zone_id",
+            },
+            waterZoneId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "water_zone_id",
+            },
+            buildingId: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                field: "building_id",
+            },
+            roomNo: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: "room_no",
+            },
+            roomType: {
+                type: DataTypes.ENUM,
+                values: ["single", "family_1", "family_2"],
+                field: "room_type",
+            },
+            electricityNo: {
+                type: DataTypes.STRING,
+                field: "electricity_no",
+            },
+            electricityMeterNo: {
+                type: DataTypes.STRING,
+                field: "electricity_meter_no",
+            },
+            waterNo: {
+                type: DataTypes.STRING,
+                field: "water_no",
+            },
+            waterMeterNo: {
+                type: DataTypes.STRING,
+                field: "water_meter_no",
+            },
+            status: {
+                type: DataTypes.ENUM,
+                values: ["empty", "not_empty"],
+                field: "status",
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "created_at",
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: DataTypes.NOW,
+                field: "updated_at",
+            },
+        });
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropAllTables()
-        await queryInterface.removeIndex('users', ['username', 'email'])
-        await queryInterface.dropAllEnums()
-    }
-}
+        await queryInterface.dropAllTables();
+        await queryInterface.removeIndex("users", ["username", "email"]);
+        await queryInterface.dropAllEnums();
+    },
+};
